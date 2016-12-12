3780eed764b13dfdb7693205ecb81b85185d24e2
shareinto <56471924@qq.com>
2016-12-12 18:06:21 +0800

    renamed:    Polymorphic1/pom.xml -> Polymorphic/pom.xml 	new file:   Polymorphic/src/main/java/BlackHorse.java 	new file:   Polymorphic/src/main/java/Horse.java 	new file:   Polymorphic/src/main/java/Mozi.java 	new file:   Polymorphic/src/main/java/Program.java 	new file:   Polymorphic/src/main/java/WhiteHorse.java

---
+++ lession1/Polymorphic/src/main/java/BlackHorse.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/12/12.
+ */
+public class BlackHorse extends Horse {
+    @Override
+    public void eat() {
+        System.out.println("黑马吃草");
+    }
+}
---
+++ lession1/Polymorphic/src/main/java/WhiteHorse.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/12/12.
+ */
+public class WhiteHorse extends Horse {
+    @Override
+    public void eat() {
+        System.out.println("白马吃草");
+    }
+}
